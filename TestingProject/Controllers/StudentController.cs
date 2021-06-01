b7b53554cd33dde0d4411578dd2f2590ac2ec3c8
thara99 <it19148014@my.sliit.lk>
2021-06-01 17:40:32 +0530

    first

---
+++ TestingProject/Controllers/StudentController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TestingProject.Models;
+
+namespace TestingProject.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "slmoEsLuT4TwEcsd5oOu6gsy5nbQtyG7JCXLOMvI",
+            BasePath = "https://asp-mvc-b412a-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        // GET: Student
+        public ActionResult Index()
+        {
+
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Student");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Student>();
+            foreach(var item in data)
+            {
+                list.Add(JsonConvert.DeserializeObject<Student>(((JProperty)item).Value.ToString()));
+            }
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                AddStudentToFirebase(student);
+                ModelState.AddModelError(String.Empty, "Added Successfully");
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError(String.Empty, ex.Message);
+            }
+            return View();
+        }
+      
+            
+        
+
+        private void AddStudentToFirebase(Student student)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            var data = student;
+            PushResponse response = client.Push("Student/", data);
+            data.student_id = response.Result.name;
+            SetResponse setResponse = client.Set("Student/" + data.student_id, data);
+        }
+
+        [HttpGet]
+        public ActionResult Detail(String id)
+        {
+
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Student/"+id); 
+            dynamic data = JsonConvert.DeserializeObject<Student>(response.Body);
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(String id)
+        {
+
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Student/" + id);
+            dynamic data = JsonConvert.DeserializeObject<Student>(response.Body);
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+
+            client = new FireSharp.FirebaseClient(config);
+            SetResponse response = client.Set("Student/" + student.student_id,student);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(String id)
+        {
+
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Delete("Student/" + id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TestingProject/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestingProject.Models
+{
+    public class Student
+    {
+
+        public String student_id { get; set; }
+
+        public String fullname { get; set; }
+
+        public String degree_title { get; set; }
+
+        public String address { get; set; }
+
+        public String phone { get; set; }
+
+
+
+    }
+}
